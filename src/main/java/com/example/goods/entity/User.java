cb761ec86c99153d58ce2d2a7cd3055103de2418
Alison <271069556@qq.com>
2020-06-08 21:48:34 +0800

    修正用户实体类

--- src/main/java/com/example/goods/entity/User.java
+++ src/main/java/com/example/goods/entity/User.java
@@ -8,5 +8,14 @@ import java.util.Date;
  * @date 2020/5/25
  */
 @Data
-public class User { 	 private Integer id; 	 private String account; 	 private String password; 	 private String name; 	 private Integer age; 	 private String gender; 	 private String phone; 	 private String remark; 	 private String head; 	 private Integer departmentid; 	 private Integer roleid;
+public class User {
+ 	 private Integer id;
+ 	 private String name;
+ 	 private Integer age;
+ 	 private String gender;
+ 	 private String phone;
+ 	 private String remark;
+ 	 private String head;
+ 	 private Integer departmentid;
+ 	 private Integer roleid;
 }
